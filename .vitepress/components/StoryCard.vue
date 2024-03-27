a63b62469a0217f8ec5a339b5fb29ebf703729df
Expliyh <expliyh@outlook.com>
2024-03-27 12:33:10 +0800

    添加传说任务链接卡片

---
+++ .vitepress/components/StoryCard.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="story-card">
+    <a :href="link.url" class="card-link">
+      <div class="card-content">
+        <img :src="link.logo" alt="logo" class="card-logo"/>
+        <div class="card-info">
+          <h3 class="card-title">{{ link.title }}</h3>
+          <p class="card-description">{{ link.description }}</p>
+        </div>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StoryCard',
+  props: {
+    link: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.story-card {
+  border: 1px solid #eee;
+  border-radius: 10px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  margin: 10px;
+  transition: box-shadow 0.3s;
+  width: 100%;
+  max-width: 500px; /* Adjust the width as per requirement */
+}
+
+.story-card:hover {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.card-link {
+  display: flex;
+  text-decoration: none;
+}
+
+.card-content {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+.card-logo {
+  width: 80px; /* Adjust the size as per requirement */
+  height: 80px;
+  border-radius: 50%;
+  margin: 10px;
+  object-fit: cover;
+}
+
+.card-info {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 10px;
+  flex-grow: 1;
+}
+
+.card-title {
+  font-size: 1.2em;
+  color: #333;
+  margin: 0;
+  padding: 0;
+}
+
+.card-description {
+  font-size: 0.9em;
+  color: #666;
+  margin: 0;
+}
+</style>
--- genshin/story/index.md
+++ genshin/story/index.md
@@ -6,6 +6,56 @@ footer: true
 editLink: true
 ---
 
+<script setup>
+import StoryCard from '../../.vitepress/components/StoryCard.vue'
+
+const storyLinks= [
+  {
+    title: '纳西妲',
+    url: './nahida/',
+    logo: 'https://vip.123pan.cn/1814176066/DirectLink/%E7%BA%B3%E8%A5%BF%E5%A6%B2_Quan.jpg',
+    description: '深居净善宫的笼中之鸟，只能在梦中将世界尽收眼底。'
+  },
+  {
+    title: '测试用',
+    url: 'https://example.com',
+    logo: 'https://vip.123pan.cn/1814176066/DirectLink/%E7%BA%B3%E8%A5%BF%E5%A6%B2_Quan.jpg',
+    description: '测试用'
+  },
+]
+</script>
+
 # 原神角色传说任务
 
+<div class="container">
+<StoryCard
+  class="story-card"
+  v-for="link in storyLinks"
+  :key="link.title"
+  :link="link"
+/>
+</div>
 
+<style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 30px;
+  gap:20px;
+  justify-content: start;
+}
+@media (min-width: 600px) { /* 调整这个宽度以适应你的设计需求 */
+  .container {
+    justify-content: space-between;
+  }
+  .story-card {
+    flex: 0 1 calc(50% - 40px); /* 两列布局，减去间隙 */
+  }
+}
+
+@media (max-width: 599px) { /* 与上面的阈值匹配 */
+  .story-card {
+    flex: 0 1 100%; /* 一列布局 */
+  }
+}
+</style>
