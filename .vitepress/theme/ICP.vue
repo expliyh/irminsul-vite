39b74292d948027738a9f1a36a7edc4969597782
Expliyh <expliyh@outlook.com>
2024-03-27 12:48:13 +0800

    添加备案信息

---
+++ .vitepress/theme/ICP.vue
@@ -0,0 +1,34 @@
+<template>
+  <Layout>
+    <template #layout-bottom>
+      <div class="icp-info" v-if="showICP">
+        <a href="http://beian.miit.gov.cn/" target="_blank">鲁ICP备2023044905号</a>
+      </div>
+    </template>
+  </Layout>
+</template>
+
+<script setup>
+import {ref, onMounted} from 'vue';
+import DefaultTheme from 'vitepress/theme'
+
+const {Layout} = DefaultTheme
+
+const showICP = ref(false);
+
+onMounted(() => {
+  const host = window.location.hostname;
+
+  const domainsToShowICP = ['www.irminsul.top', 'irminsul.top'];
+
+  showICP.value = domainsToShowICP.includes(host);
+});
+</script>
+
+
+<style>
+.icp-info {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,9 @@
+// .vitepress/theme/index.js
+import DefaultTheme from 'vitepress/theme'
+import ICP from './ICP.vue'
+
+export default {
+    extends: DefaultTheme,
+    // 使用注入插槽的包装组件覆盖 Layout
+    Layout: ICP
+}
